36648c885a7425f1e54998e5b29eaa5cee4b0cb0
Alexander Royal <alexander.de.royal@gmail.com>
2023-10-27 01:12:12 -0400

    completed section 39

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,7 @@ db.once("open", () => {
     console.log("Database connected")
 });
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)];
+const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
 const seedDB = async () => {
@@ -27,7 +27,7 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location: `${cities[random1000].city},${cities[random1000].state}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
