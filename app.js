dcaa1302bd8f473e5a682843f9a0d9a827138d62
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-02 16:46:19 +0545

    completed authentication

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/campgrounds')
+const usersRoutes = require('./routes/users');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
 
 main().catch(err=>console.log(err))
-
 async function main() {
 
     await mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -32,9 +35,9 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
-app.use(express.static((path.join(__dirname), 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 const sessionConfig = {
     secret: 'thisshouldbeabettersecret!',
@@ -49,14 +52,23 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
-    next()
+    next();
 });
 
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+
+app.use('/', usersRoutes);
+app.use('/campgrounds', campgroundsRoutes)
+app.use('/campgrounds/:id/reviews', reviewsRoutes)
 
 app.get('/', (req, res) => {
     res.render('home')
